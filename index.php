97ec54cd21e48e5d275788da3570acadc6cb4fe9
Mamadian <mdjalo@condorcet93.fr>
2020-03-05 14:48:58 +0100

    ajout d'un meta

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <head>
 	<meta charset="UTF-8">
 	<title><?php print titre ?></title>
-	
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
 	
 	<link rel="stylesheet" href="css/style.css">
