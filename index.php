a5f9d3475b3fdd6dfda5325d754c56adf640d4bd
Mamadian <mdjalo@condorcet93.fr>
2020-03-05 16:45:58 +0100

    Ajout d'un meta description

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 	<meta charset="UTF-8">
 	<title><?php print titre ?></title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="Ce site est développé dans le cadre d'un prjet de bts sio">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
 	
 	<link rel="stylesheet" href="./static/css/style.css">
